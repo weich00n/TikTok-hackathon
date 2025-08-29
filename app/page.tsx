776379b929ad08b1d4526c97117fe4b663b03ae9
Vincent C <vincentc361@gmail.com>
2025-08-29 18:14:13 +0800

    Add FrontEnd

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { VoiceMessageTranscriber } from "@/components/voice-message-transcriber"
+
+export default function VoiceTranscriberApp() {
+  return (
+    <div className="min-h-screen bg-background">
+      <VoiceMessageTranscriber />
+    </div>
+  )
+}
